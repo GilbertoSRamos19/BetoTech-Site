75ef78208244359a4048f3b1483130a07c00995a
Gilberto Silva <https://github.com/GilbertoSRamos19>
2025-09-01 17:31:59 -0300

    Modificação no arquivo vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    host: "0.0.0.0", // Permite acesso externo
+    port: 5173       // Porta padrão do Vite
+  }
 });
